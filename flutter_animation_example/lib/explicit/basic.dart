827719d073efc5bdcace3a9fa3edf6dfe2700ed3
Angga <angga.saputra@coinbit.id>
2022-05-21 13:45:46 +0700

    Explicit Basic

--- flutter_animation_example/lib/explicit/basic.dart
+++ flutter_animation_example/lib/explicit/basic.dart
@@ -1,4 +1,7 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ExplicitBasicExampleScreen extends StatefulWidget {
   @override
@@ -7,11 +10,73 @@ class ExplicitBasicExampleScreen extends StatefulWidget {
 }
 
 class _ExplicitBasicExampleScreenState
-    extends State<ExplicitBasicExampleScreen> {
+    extends State<ExplicitBasicExampleScreen> with SingleTickerProviderStateMixin {
+
+  late AnimationController animationController;
+  late Animation<double> fanRotationTween;
+
+
+  @override
+  initState(){
+    animationController = AnimationController(vsync: this, duration: Duration(milliseconds: 1750));
+    fanRotationTween = Tween(begin: 0.0, end: 360.0).animate(animationController);
+    super.initState();
+
+    animationController..forward()
+      ..addListener(() {
+          setState((){});
+        if (animationController.isCompleted) {
+          animationController.repeat();
+        }
+      });
+  }
+
+  dispose(){
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Center(
+      child: Container(
+        width: 170,
+        height: 84,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(14),
+            color: Colors.blueGrey
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: Column(
+          children: [
+            Container(
+                width: double.infinity,
+                color: Color.fromRGBO((fanRotationTween.value * 0.75).toInt(),(fanRotationTween.value * 0.2).toInt(),(fanRotationTween.value).toInt(),1),
+                child: Center(child: Text('RTX 3090 SUper', style: GoogleFonts.staatliches(color: Colors.white),))),
+            Expanded(
+              child: Stack(
+                children: [
+                  Positioned(
+                    left: 4,
+                    bottom: 4.0,
+                    child: Transform.rotate(angle: fanRotationTween.value,
+                      child: Icon(Icons.settings, size: 64, color: Colors.yellow,),
+                    ),
+                  ),
+                  Positioned(
+                    right: 4,
+                    bottom: 4.0,
+                    child: Transform.rotate(angle: fanRotationTween.value,
+                      child: Icon(Icons.settings, size: 64, color: Colors.yellow,),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
  
